fd4f12eeb1c5c9d367a6bc6f8a9c9c3662000d46
Cliff Moon <cliff@galva.desk.hq.powerset.com>
2008-04-02 14:37:05 -0700

    moar tests

--- src/etest/membership_test.erl
+++ src/etest/membership_test.erl
@@ -1,4 +1,22 @@
 -include_lib("include/eunit/eunit.hrl").
 
-int_join_ring_test() ->
-	true.
+
+nearest_server_test() ->
+	1 = nearest_server(0, [1,2,3]),
+	2 = nearest_server(1, [1,2,3]),
+	3 = nearest_server(2, [1,2,3]),
+	first = nearest_server(3, [1,2,3]),
+	io:format("ooga~n"),
+	State = mock_state([{1,one},{2,two},{3,three}]),
+	two = nearest_server(1, State),
+	one = nearest_server(6, State).
+
+mock_state(Tuples) ->
+	mock_state(Tuples, [], dict:new()).
+	
+mock_state([], HashRing, Table) ->
+	#membership{hash_ring=HashRing,member_table=Table};
+	
+mock_state([{Code,Name}|Tail], HashRing, Table) ->
+	mock_state(Tail, add_nodes_to_ring([Code], HashRing), dict:store(Code, Name, Table)).
+	
