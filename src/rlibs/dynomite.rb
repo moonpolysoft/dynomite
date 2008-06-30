e99310978fdb65046b8b011816dcbc2946f699b1
Cliff Moon <cliff@moonpolysoft.com>
2008-06-30 09:05:43 -0700

    work on the protocol handlers and ruby library.

---
+++ src/rlibs/dynomite.rb
@@ -0,0 +1,39 @@
+# ruby protocol handler for dynomite.
+
+class Dynomite
+  DEFAULTS = {
+    :port => 11211,
+    :host => 'localhost',
+    
+  }
+  
+  
+  def initialize(options={})
+    
+  end
+  
+  def get
+    
+  end
+  
+  def put
+    
+  end
+  
+  def has_key
+    
+  end
+  
+  def delete
+    
+  end
+  
+  def get_to_file
+    
+  end
+  
+  def put_from_file
+    
+  end
+  
+end
---
+++ src/rspecs/spec_helper.rb
@@ -0,0 +1,62 @@
+require 'spec'
+require 'mocha'
+require 'stubba'
+require File.dirname(__FILE__) + "/../lib/datamuncher"
+
+Spec::Runner.configure do |config|
+  # config.before(:all) {}
+  config.before(:each) {
+    
+  }
+  # config.after(:all) {}
+  config.after(:each) {
+    
+  }
+end
+
+
+def load_spec(filename)
+  YAML.load_file(File.dirname(__FILE__) + "/#{filename}")
+end
+
+class Object
+  class Bypass
+    instance_methods.each do |m|
+      undef_method m unless m =~ /^__/
+    end
+
+    def initialize(ref)
+      @ref = ref
+    end
+  
+    def method_missing(sym, *args)
+      @ref.__send__(sym, *args)
+    end
+  end
+  
+  class Assigns
+    instance_methods.each do |m|
+      undef_method m unless m =~ /^__/
+    end
+    
+    def initialize(ref)
+      @ref = ref
+    end
+    
+    def method_missing(sym, *args)
+      if sym.to_s =~ /^(.+)=$/
+        @ref.instance_variable_set("@#{$1}", args.length == 1 ? args.first : args)
+      else
+        @ref.instance_variable_get("@#{sym}")
+      end
+    end
+  end
+
+  def bypass
+    Bypass.new(self)
+  end
+  
+  def assigns
+    Assigns.new(self)
+  end
+end
