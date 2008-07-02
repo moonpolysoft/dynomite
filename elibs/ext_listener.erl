06b0438b5a395a3bbdadc378e3a577baf3f9fdc7
Cliff Moon <cliff@moonpolysoft.com>
2008-07-02 11:35:28 -0700

    external protocol work

--- elibs/ext_listener.erl
+++ elibs/ext_listener.erl
@@ -6,7 +6,7 @@
 -endif.
 
 start() ->
-	{ok, Listen} = gen_tcp:listen(11211, [binary, {active, once}, {packet, 0}]),
+	{ok, Listen} = gen_tcp:listen(11211, [binary, {active, false}, {packet, 0}]),
 	spawn(fun() -> par_connect(Listen) end).
 	
 par_connect(Listen) ->
@@ -16,24 +16,54 @@ par_connect(Listen) ->
 	loop(Socket).
 	
 loop(Socket) ->
-	receive
-		{tcp, Socket, Bin} ->
-			Cmd = parse_command(Bin),
-			io:format("got this: ~p~n", [Cmd]),
-			loop(Socket)
-	end.
-	
-parse_command(<<"get", $\s, Key/binary>>) -> {get, Key};
+  Cmd = read_section(Socket),
+  io:format("received command: ~p~n", [Cmd]),
+  execute_command(Cmd, Socket),
+  loop(Socket).
 
-parse_command(<<"set", $\s, Data/binary>>) -> 
-	{Key, Value} = split_next_space(Data),
-	{set, Key, Value}.
-	
-split_next_space(Binary) ->
-	split_next_space([], Binary).
-	
-split_next_space(Head, <<$\s, Rest/binary>>) ->
-	{list_to_binary(lists:reverse(Head)), Rest};	
+execute_command("get", Socket) ->
+  Length = read_length(Socket),
+  Key = read_data(Socket, Length),
+  case mediator:get(Key) of
+    {failure, Reason} -> send_failure(Socket, Reason);
+    {Context, Values} -> send_get(Socket, Context, Values)
+  end.
+  
+send_failure(Socket, Reason) ->
+  gen_tcp:send(Socket, "fail "),
+  gen_tcp:send(Socket, Reason),
+  gen_tcp:send(Socket, $\n).
+  
+send_get(Socket, Context, Values) ->
+  ContextBin = term_to_binary(Context)
+  write_binary(ContextBin, $\s),
+  ItemLength = length(Values),
+  lists:foldl(fun(Value, Acc) ->
+    case Acc of
+      ItemLength-1 -> write_binary(Socket, Value, $\n);
+      _ -> write_binary(Socket, Value, $\s)
+    end,
+    Acc+1
+  end, 0, Values).
+  
+read_length(Socket) ->
+  list_to_integer(read_section(Socket)).
 
-split_next_space(Head, <<Char:1/binary, Rest/binary>>) ->
-	split_next_space([Char|Head], Rest).
+read_section(Socket) ->
+  read_command([], Socket);
+
+read_section(Cmd, Socket) ->
+  case gen_tcp:recv(Socket, 1) of
+    {ok, <<$\s>>} -> lists:reverse(Cmd);
+    {ok, <<Char>>} -> [Char, Cmd]
+  end.
+  
+write_binary(Socket, Bin, Term) ->
+  Length = byte_size(Bin),
+  write_length(Socket, Length),
+  gen_tcp:send(Socket, Bin),
+  gen_tcp:send(Socket, Term).
+  
+write_length(Socket, Length) ->
+  gen_tcp:send(Socket, integer_to_list(Length)),
+  gen_tcp:send(Socket, $\s).
