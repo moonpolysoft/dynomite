add9b78e329f880859ef8b33bcfab76894e93f8b
Cliff Moon <cliff@moonpolysoft.com>
2008-07-10 20:04:19 -0700

    ability to name a node to connect to

--- elibs/dynomite.erl
+++ elibs/dynomite.erl
@@ -4,4 +4,8 @@
 
 start() ->
   application:load(dynomite),
+  case application:get_key(dynomite, node) of
+    undefined -> true,
+    {ok, Node} -> net_adm:ping(Node)
+  end,
   application:start(dynomite).
