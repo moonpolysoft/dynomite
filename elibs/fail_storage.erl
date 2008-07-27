0d5f772fdefea82b5b7457b9e58d6c0c083d0ee2
Cliff Moon <cliff@moonpolysoft.com>
2008-07-27 11:00:49 -0700

    pass through fold for fail storage

--- elibs/fail_storage.erl
+++ elibs/fail_storage.erl
@@ -1,11 +1,13 @@
 -module (fail_storage).
--export ([open/2, close/1, get/2, put/4, has_key/2, delete/2]).
+-export ([open/2, close/1, get/2, fold/3, put/4, has_key/2, delete/2]).
 
 % we ignore the name, since it can't really help us.
 open(_, _) -> dict:new().
 
 % noop
 close(_Table) -> ok.
+
+fold(_, _, Acc) -> Acc.
 
 put(_Key, _Context, _Value, _Table) ->
 	{failure, "Sysadmin accidentally destroyed pager with a large hammer."}.
