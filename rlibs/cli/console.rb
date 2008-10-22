d7d37cf1e15910f984cbbd5e21cfb22636c297d0
Cliff Moon <cliff@moonpolysoft.com>
2008-10-22 15:54:16 -0700

    a remsh console

---
+++ rlibs/cli/console.rb
@@ -0,0 +1,22 @@
+options = {}
+options[:port] = 11222
+options[:databases] = ''
+options[:config] = ''
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: dynomite start [options]"
+
+  contents =  File.read(File.dirname(__FILE__) + "/shared/common.rb")
+  eval contents
+
+end.parse!
+
+cookie = Digest::MD5.hexdigest(options[:cluster] + "NomMxnLNUH8suehhFg2fkXQ4HVdL2ewXwM")
+
+str = "erl 
+  -sname remsh_#{$$} \
+  -remsh #{options[:name]}@#{`hostname -s`} \
+  -hidden \
+  -setcookie #{cookie}"
+puts str
+exec str
