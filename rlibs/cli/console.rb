1b2d53f3c743aeae7cf71dde26c3ff484303ee69
Cliff Moon <cliff@moonpolysoft.com>
2008-10-24 14:32:25 -0700

    cleaned up newlines from the console script

--- rlibs/cli/console.rb
+++ rlibs/cli/console.rb
@@ -13,10 +13,6 @@ end.parse!
 
 cookie = Digest::MD5.hexdigest(options[:cluster] + "NomMxnLNUH8suehhFg2fkXQ4HVdL2ewXwM")
 
-str = "erl 
-  -sname remsh_#{$$} \
-  -remsh #{options[:name]}@#{`hostname -s`} \
-  -hidden \
-  -setcookie #{cookie}"
+str = "erl -sname remsh_#{$$} -remsh #{options[:name]}@#{`hostname -s`.chomp} -hidden -setcookie #{cookie}"
 puts str
 exec str
