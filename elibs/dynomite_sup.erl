0d231b9c82cec6527b30cb7ffc7f34a7a33b5535
Cliff Moon <cliff@moonpolysoft.com>
2008-10-24 14:29:32 -0700

    add stat server to the startup sequence

--- elibs/dynomite_sup.erl
+++ elibs/dynomite_sup.erl
@@ -50,13 +50,13 @@ start_link(Config, Options) ->
 init([Config, Options]) ->
     {ok,{{one_for_one,10,1}, [
 			{configuration, {configuration,start_link,[Config]}, permanent, 1000, worker, [configuration]},
+			{stats_server, {stats_server,start_link,[]}, permanent, 1000, worker, [stats_server]},
       {storage_server_sup, {storage_server_sup,start_link,[Config]}, permanent, 10000, supervisor, [storage_server_sup]},
       {sync_manager, {sync_manager,start_link,[]}, permanent, 1000, worker, [sync_manager]},
       {sync_server_sup, {sync_server_sup,start_link,[Config]}, permanent, 10000, supervisor, [sync_server_sup]},
       {membership, {membership,start_link,[Config]}, permanent, 1000, worker, [membership]},
       {mediator, {mediator,start_link,[Config]}, permanent, 1000, worker, [mediator]},
       {dynomite_web, {dynomite_web,start,[Options]}, permanent, 1000, worker, [dynomite_web]},
-      %{ext_listener, {ext_listener,start_link,[Config]}, permanent, 1000, worker, [ext_listener]}
       {socket_server, {socket_server,start_link,[Config]}, permanent, 1000, worker, [socket_server]}
     ]}}.
 
