1b0c195fa913ed69a63128f6c77f97e02573aa04
Jason Pellerin <jhp@jpdesk.(none)>
2008-11-17 14:53:17 -0500

    Added python thrift client files
    Signed-off-by: Jason Pellerin <jhp@jpdesk.(none)>

---
+++ test.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append("gen-py")
+from dynomite import Dynomite
+from dynomite.ttypes import *
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+# Make socket
+transport = TSocket.TSocket('localhost', 9200)
+
+# Buffering is critical. Raw sockets are very slow
+transport = TTransport.TBufferedTransport(transport)
+
+# Wrap in a protocol
+protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+client = Dynomite.Client(protocol)
+transport.open()
+
+print client.put("b", None, "b value")
+print client.get("b")
+
+r = client.get("a")
+print r
+
+print client.put("a", r.context, "a new value")
+print client.get("a")
+
+print client.put("a", r.context, "a newer new value")
+print client.get("a")
+
+transport.close()
