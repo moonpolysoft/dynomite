8cdd91b52225107abd1aa5bc43bcbfd25cf7cc17
Cliff Moon <cliff@moonpolysoft.com>
2009-01-18 10:06:05 -0800

    common header from the backups branch

---
+++ include/common.hrl
@@ -0,0 +1,4 @@
+
+-define(fmt(Msg, Args), lists:flatten(io_lib:format(Msg, Args))).
+-define(infoFmt(Msg, Args), error_logger:info_msg(Msg, Args)).
+-define(infoMsg(Msg), error_logger:info_msg(Msg)).
