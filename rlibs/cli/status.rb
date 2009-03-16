b7aad6d70f0d27a9262f424b5284dc3590379c72
Cliff Moon <cliff@moonpolysoft.com>
2009-03-16 10:51:21 -0700

    status command

---
+++ rlibs/cli/status.rb
@@ -0,0 +1,14 @@
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: dynomite stop [options]"
+
+  contents =  File.read(File.dirname(__FILE__) + "/shared/common.rb")
+  eval contents
+end.parse!
+
+cookie = Digest::MD5.hexdigest(options[:cluster] + "NomMxnLNUH8suehhFg2fkXQ4HVdL2ewXwM")
+
+str = %Q(erl -smp -sname console_#{$$} -hidden -setcookie #{cookie} -pa #{ROOT}/ebin/ -run commands start -run erlang halt -noshell -node #{options[:name]}@#{`hostname -s`.chomp} -m membership -f status)
+puts str
+exec str
