eb55094def7bf1b92ba6e9703c636aa3255f7b79
Cliff Moon <cliff@moonpolysoft.com>
2009-04-09 10:19:46 -0700

    show args on command line

--- deps/thrift/build/buildtargets.mk
+++ deps/thrift/build/buildtargets.mk
@@ -7,8 +7,7 @@ ERLC = erlc $(ERLC_WFLAGS) $(ERLC_FLAGS)
 ERL = erl -boot start_clean
 
 $(EBIN)/%.beam: $(ESRC)/%.erl
-	@echo "   ERLC  $<"
-	@$(ERLC) $(ERL_FLAGS) $(ERL_COMPILE_FLAGS) -o$(EBIN) $<
+	$(ERLC) $(ERL_FLAGS) $(ERL_COMPILE_FLAGS) -o$(EBIN) $<
 
 .erl.beam:
 	$(ERLC) $(ERL_FLAGS) $(ERL_COMPILE_FLAGS) -o$(dir $@) $<
