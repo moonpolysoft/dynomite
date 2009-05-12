1f42b55ae9fde3c9450d8306d50b3cab449d9e2f
Cliff Moon <cliff@moonpolysoft.com>
2009-05-11 20:51:35 -0700

    define debug if no test

--- include/common.hrl
+++ include/common.hrl
@@ -2,3 +2,8 @@
 -define(fmt(Msg, Args), lists:flatten(io_lib:format(Msg, Args))).
 -define(infoFmt(Msg, Args), error_logger:info_msg(Msg, Args)).
 -define(infoMsg(Msg), error_logger:info_msg(Msg)).
+
+-ifndef(TEST).
+-define(?debugMsg(Msg), ok).
+-define(?debugFmt(Msg, Args), ok).
+-endif.
